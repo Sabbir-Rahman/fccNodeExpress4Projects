7fedc2b733f928852250efeba6cdcb1af63d962a
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-01-31 00:25:36 +0600

    initial setup

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express();
+
+const port = 5000
+
+app.get('/',(req,res)=> {
+    res.send('Task manager app')
+})
+
+app.listen(port, console.log(`Server is listening on port ${port}`))
